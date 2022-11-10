db08b5d4868e37bd836d4d99bf2e7dbc6b174cb1
Sofy Nabillah <sofynabillah368@gmail.com>
2022-11-10 13:47:06 +0700

    commit1

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  const LoginView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Container(margin: EdgeInsets.all(Get.height * 0.1), decorations),
+    );
+  }
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,20 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const FRIENDS = _Paths.FRIENDS;
+  static const PROFILE = _Paths.PROFILE;
+  static const TASK = _Paths.TASK;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const FRIENDS = '/friends';
+  static const PROFILE = '/profile';
+  static const TASK = '/task';
+}
