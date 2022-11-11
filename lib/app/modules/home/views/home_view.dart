a58f807e07c1492967222a5bab2d3ff15972da95
Sofy Nabillah <sofynabillah368@gmail.com>
2022-11-11 18:27:30 +0700

    commit home & sidebar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+import 'package:task_management_app/app/utils/widget/header.dart';
+import 'package:task_management_app/app/utils/widget/sideBar.dart';
+
 
 import '../controllers/home_controller.dart';
 
@@ -9,16 +15,34 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      backgroundColor: Colors.grey[100],
+        body: Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: SideBar(),
+            ),
+            Expanded(
+              flex: 15,
+              child: Column(
+                children: [
+                  header(),
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.all(50),
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ))
+      
     );
   }
 }
---
+++ lib/app/utils/widget/sideBar.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+
+class SideBar extends StatelessWidget {
+  const SideBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      elevation: 0,
+      child: Container(
+        height: Get.height,
+        color: Colors.grey[100],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //logo
+              Container(
+                height: 100,
+                padding: const EdgeInsets.only(top: 30),
+                width: double.infinity,
+                alignment: Alignment.topCenter,
+                child: const Image(
+                  image: AssetImage('assets/icons/icon.png'),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                height: 100,
+                child: Center(
+                  child: InkWell(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 100,
+                          decoration: Get.currentRoute == '/home'
+                              ? BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                )
+                              : const BoxDecoration(),
+                          child: Icon(
+                            Get.currentRoute == '/home'
+                                ? Ionicons.desktop
+                                : Ionicons.desktop_outline,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          'Home',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () => Get.toNamed(Routes.HOME),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                child: Center(
+                  child: InkWell(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 100,
+                          decoration: Get.currentRoute == '/task'
+                              ? BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                )
+                              : const BoxDecoration(),
+                          child: Icon(
+                            Get.currentRoute == '/task'
+                                ? Ionicons.cube
+                                : Ionicons.cube_outline,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          'Task',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () => Get.toNamed(Routes.TASK),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                child: Center(
+                  child: InkWell(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 100,
+                          decoration: Get.currentRoute == '/friends'
+                              ? BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                )
+                              : const BoxDecoration(),
+                          child: Icon(
+                            Get.currentRoute == '/friends'
+                                ? Ionicons.heart
+                                : Ionicons.heart_outline,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          'Friends',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () => Get.toNamed(Routes.FRIENDS),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+                child: Center(
+                  child: InkWell(
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 40,
+                          width: 100,
+                          decoration: Get.currentRoute == '/profile'
+                              ? BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  color: Colors.white,
+                                )
+                              : const BoxDecoration(),
+                          child: Icon(
+                            Get.currentRoute == '/profile'
+                                ? Ionicons.person
+                                : Ionicons.person_outline,
+                            color: Colors.black,
+                            size: 30,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        const Text(
+                          'Profile',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () => Get.toNamed(Routes.PROFILE),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
